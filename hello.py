2865f69cbe8c11f61717aab321eea30233d958f6
mdrafi276 <mdrafi37473@gmail.com>
2024-02-07 14:31:18 +0600

    problem solve on list

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
-number = [1, 2, 3, 4, 5]
-num = number.append(7)
-print(num)
-print(number)
+# number = [1, 2, 3, 4, 5]
+# num = number.append(7)
+# print(num)
+# print(number)
 
-amount = {
-    "name":"Rafi",
-    "price":10,
-}
-print(amount.pop(1))
+# amount = {
+#     "name":"Rafi",
+#     "price":10,
+# }
+# print(amount.pop(1))
+
+mylist = ["apple", "banana", "cherry"]
+print(mylist[1])
