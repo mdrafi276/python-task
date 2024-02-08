8471390d7194f1c26f687725e20b948b03b6d2b9
mdrafi276 <mdrafi37473@gmail.com>
2024-02-09 00:33:52 +0600

    today commit commit five

--- hello.py
+++ hello.py
@@ -42,5 +42,10 @@ data = [5, 6, 8, 9, 1, 3, 4]
 print(data[::-2])
 data = [5, 6, 8, 9, 1, 3, 4]
 print(data[::-2])
+print(data[::-2])
+data = [5, 6, 8, 9, 1, 3, 4]
+print(data[::-2])
+data = [5, 6, 8, 9, 1, 3, 4]
+print(data[::-2])
 
 
