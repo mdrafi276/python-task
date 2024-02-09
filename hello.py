64537121e01920d12ad91f972074110e79c27c4b
mdrafi276 <mdrafi37473@gmail.com>
2024-02-10 00:04:12 +0600

    create new functions

--- hello.py
+++ hello.py
@@ -1,3 +1,28 @@
+# number = [1, 2, 3, 4, 5]
+# num = number.append(7)
+# print(num)
+# print(number)
+
+# amount = {
+#     "name":"Apple",
+#     "price":10,
+# }
+# print(amount.pop(1))
+
+# mylist = ["apple", "banana", "cherry"]
+# print(mylist[1])
+# my_list = ['rafi', "shakib", "rifat", "arafat"]
+# print(type(my_list))
+# list1 = ["abc", 34, True, 40, "male"]
+# print(list1)
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[2:6])
+# thislist = ["apple", "banana", "cherry"]
+# for x in thislist:
+#   print(x)
+# thislist1 = ["apple", "banana", "cherry"]
+# for x in range(len(thislist1)):
+#     print(x)
 
 # for x in range(2, 4):
 #   print()
@@ -35,7 +60,16 @@
 # print(sum)
 
 
+# def print_hello():
+    
+#     print("hello")
+   
 
+# print_hello()
+# print_hello()
+# print_hello()
+# print_hello()
+# print_hello()
 
 # avarage of 3 numbers
 
@@ -116,10 +150,10 @@
 # Hint: use list & index as parameters.
 
 
-# def print_list(list, idx=0):
-#     if(idx == len(list)):
-#         return
-#     print(list[idx])
-#     print_list(list, idx+1)
-# fruits = ["mango"," litchi ", "apple", "banana"]
-# print_list(fruits)
+def print_list(list, idx=0):
+    if(idx == len(list)):
+        return
+    print(list[idx])
+    print_list(list, idx+1)
+fruits = ["mango"," litchi ", "apple", "banana"]
+print_list(fruits)
