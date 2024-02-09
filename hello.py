d39c7f15a2e2b2fb44683a2fa43561a7715b4207
mdrafi276 <mdrafi37473@gmail.com>
2024-02-10 00:09:24 +0600

    i

--- hello.py
+++ hello.py
@@ -150,10 +150,10 @@
 # Hint: use list & index as parameters.
 
 
-def print_list(list, idx=0):
-    if(idx == len(list)):
-        return
-    print(list[idx])
-    print_list(list, idx+1)
-fruits = ["mango"," litchi ", "apple", "banana"]
-print_list(fruits)
+# def print_list(list, idx=0):
+#     if(idx == len(list)):
+#         return
+#     print(list[idx])
+#     print_list(list, idx+1)
+# fruits = ["mango"," litchi ", "apple", "banana"]
+# print_list(fruits)
