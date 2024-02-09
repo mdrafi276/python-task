a555965be5c9f8aafb9f691da4ffaa3a8a1d9f77
mdrafi276 <mdrafi37473@gmail.com>
2024-02-09 23:57:14 +0600

    create new function

--- hello.py
+++ hello.py
@@ -1,17 +1,4 @@
 
-# my_list = ['rafi', "shakib", "rifat", "arafat"]
-# print(type(my_list))
-# list1 = ["abc", 34, True, 40, "male"]
-# print(list1)
-# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-# print(thislist[2:6])
-# thislist = ["apple", "banana", "cherry"]
-# for x in thislist:
-#   print(x)
-# thislist1 = ["apple", "banana", "cherry"]
-# for x in range(len(thislist1)):
-#     print(x)
-
 # for x in range(2, 4):
 #   print()
 # def add(num1, num2):
