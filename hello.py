b7bd7340207ed5256a1e8546fbec7448d133a5d0
mdrafi276 <mdrafi37473@gmail.com>
2024-02-11 23:22:16 +0600

    add

--- hello.py
+++ hello.py
@@ -172,15 +172,15 @@
 # print(car1.brand)
 
 
-class Student :
-    def __inti__(self):
-        print("adding new student in Database")
-    def __init__(self, fullname, marks):
-        self.name = fullname
-        self.marks = marks
-        print("adding new student in database..")
+# class Student :
+#     def __inti__(self):
+#         print("adding new student in Database")
+#     def __init__(self, fullname, marks):
+#         self.name = fullname
+#         self.marks = marks
+#         print("adding new student in database..")
 
-s1 = Student("karan", 97)
-print(s1.name, s1.marks)
-s2 = Student("arjun", 88)
-print(s2.name, s2.marks)
+# s1 = Student("karan", 97)
+# print(s1.name, s1.marks)
+# s2 = Student("arjun", 88)
+# print(s2.name, s2.marks)
